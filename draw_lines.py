a59590f1131971783da442d903ced98d1b03f438
Anders Hoff <inconvergent@gmail.com>
2016-07-21 00:26:07 +0200

    draw lines and tris

---
+++ draw_lines.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+
+from xy.device import Device
+
+
+def main(args):
+  from modules.utils import get_paths_from_file as get
+
+  fn = args.fn
+  paths = get(fn, 0.95, spatial_concat = True, spatial_concat_eps=1e-4)
+
+  with Device() as device:
+    device.do_paths(paths)
+
+
+if __name__ == '__main__':
+
+  import argparse
+
+  parser = argparse.ArgumentParser()
+  parser.add_argument(
+    '--fn',
+    type=str,
+    required=True
+  )
+
+  args = parser.parse_args()
+  main(args)
+
---
+++ draw_tris.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+
+from xy.device import Device
+
+
+def main(args):
+
+  from modules.utils import get_tris_from_file as get
+  fn = args.fn
+  paths = get(fn, 0.90, spatial_concat=True, spatial_concat_eps=1.e-13)
+
+  with Device() as device:
+    device.do_paths(paths)
+
+
+if __name__ == '__main__':
+  import argparse
+
+  parser = argparse.ArgumentParser()
+  parser.add_argument(
+    '--fn',
+    type=str,
+    required=True
+  )
+
+  args = parser.parse_args()
+  main(args)
+
