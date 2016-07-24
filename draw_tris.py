6bf0e95a551fdf0fb50f0ab513d0ab74257a1629
Anders Hoff <inconvergent@gmail.com>
2016-07-25 01:19:58 +0200

    almost full size.

--- draw_tris.py
+++ draw_tris.py
@@ -9,7 +9,7 @@ def main(args):
 
   from modules.utils import get_tris_from_file as get
   fn = args.fn
-  paths = get(fn, 0.93, spatial_concat=True, spatial_concat_eps=1.e-13)
+  paths = get(fn, 0.99, spatial_concat=True, spatial_concat_eps=1.e-13)
 
   with Device(penup=0.4) as device:
     device.do_paths(paths)
